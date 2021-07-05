cc6ec61d812f7a5c3f578b3d73fd9cc4ee0cd35e
Farid AMOUSSA <amousssa.farid2@gmail.com>
2021-07-06 00:49:02 +0100

    Implement Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
