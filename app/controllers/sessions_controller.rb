faf5a0582c90b59114e21a677200b3a876a59f9d
Farid AMOUSSA <amousssa.farid2@gmail.com>
2021-07-06 05:09:06 +0100

    Authentification code end

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      raise
+    else
+      flash.now[:danger] = "Credentials not recognized"
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/", to: "users#new"
+  get "/", to: "sessions#new"
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :edit, :update, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
